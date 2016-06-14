ec2a09cf789cbf57facd201b124d81e9daf50eb6
Carlo <carlo21cipriani@gmail.com>
2016-06-14 19:26:39 +0200

    Undestanding if a sentence is a question or not

---
+++ question_understanding.py
@@ -0,0 +1,15 @@
+from textblob import TextBlob
+kind_of_verbs=["VB","VBZ","VBP","VBD","MD"]
+wh_starts=["WRB","WP","WDT"]
+sent=input("Tell me something: ")
+sent=TextBlob(sent)
+if "?" in sent:
+    print("This is a question")
+else:
+    tag=sent.tags
+    if tag[0][1]in kind_of_verbs:
+        print("This is a question")
+    if tag[0][1]in wh_starts:
+        print("This is a question")
+    else:
+        print("This is not a question")
